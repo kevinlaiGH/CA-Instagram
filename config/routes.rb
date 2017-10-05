ff4fb3ba160b6a70a35f006d9bf242275f9cab2e
Kevin Lai <kevinlai@kevins-mbp.ait-sydney.net.au>
2017-10-05 11:19:27 +1100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # get 'landing/index'
   root "home#landing"
 
+  resources :photos, only: [:new, :create, :index, :edit, :update]
+
+  root 'photos#index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
